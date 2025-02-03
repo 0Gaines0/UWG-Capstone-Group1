015ba3a19faa0d8c1710a7cbf8a4929b721da690
Stefan <ssrrtteeee@gmail.com>
2025-02-03 13:33:46 -0500

    Revert "Fixed Missmatch Between Database And Webapp"
    This reverts commit c709a837ab7e4a94f8a9feb2a1891ae8d36f8146.

--- code/ticket_system/ticket_system_database/dbo/Tables/user.sql
+++ code/ticket_system/ticket_system_database/dbo/Tables/user.sql
@@ -1,9 +1,9 @@
-﻿CREATE TABLE [dbo].[users] (
+﻿CREATE TABLE [dbo].[user] (
     [Id]       INT            NOT NULL,
-    [userId]  NVARCHAR (50)  NOT NULL,
+    [user_id]  NVARCHAR (50)  NOT NULL,
     [username] NVARCHAR (MAX) NOT NULL,
     [password] NVARCHAR (MAX) NOT NULL,
     PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [AK_user_id_key] UNIQUE NONCLUSTERED ([userId] ASC)
+    CONSTRAINT [AK_user_id_key] UNIQUE NONCLUSTERED ([user_id] ASC)
 );
 
