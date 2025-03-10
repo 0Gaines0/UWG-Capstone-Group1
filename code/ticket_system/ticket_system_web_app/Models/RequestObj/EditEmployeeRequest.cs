457b916039eafab388dc6066b160e36837db4796
ssrrtteeee <ssrrtteeee@gmail.com>
2025-03-10 03:28:11 -0400

    Added Edit Employee Button And Verification
    * All c# methods in employee controller are now authenticated by checking if the current user is logged in and is a admin.
    * Added edit employee button and reworked the CreateEmployeeModal to also work for editing.
    * Added some documentation to the employee controller.
    * Noticed that code to get group managers doesn't let you select regular group members.

---
+++ code/ticket_system/ticket_system_web_app/Models/RequestObj/EditEmployeeRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ticket_system_web_app.Models.RequestObj
+{
+    public class EditEmployeeRequest
+    {
+        public string? OriginalUsername { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
