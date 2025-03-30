13834b66053a480e37f14fa1ac7ffc5932af0500
Jacob Wilson <jw00358@my.westga.edu>
2025-03-29 21:22:11 -0400

    Added auth tokens
    Added randomly generated authentication tokens to ActiveEmployee.

--- code/ticket_system/ticket_system_web_app/Models/ActiveEmployee.cs
+++ code/ticket_system/ticket_system_web_app/Models/ActiveEmployee.cs
@@ -1,10 +1,22 @@
-﻿namespace ticket_system_web_app.Models
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace ticket_system_web_app.Models
 {
     /// <summary>
     /// ActiveEmployee class
     /// </summary>
     public static class ActiveEmployee
     {
+        private const int AUTH_TOKEN_LENGTH = 32;
+
+        /// <summary>
+        /// Gets the authentication token.
+        /// </summary>
+        /// <value>
+        /// The authentication token.
+        /// </value>
+        public static string AuthToken { get; private set; } = string.Empty;
+
         /// <summary>
         /// Gets or sets the employee.
         /// </summary>
@@ -25,6 +37,7 @@
                 throw new ArgumentNullException(nameof(employee), "input must not be null");
             }
             Employee = employee;
+            generateAuthToken(new Random());
         }
 
         /// <summary>
@@ -33,7 +46,53 @@
         public static void LogoutCurrentEmployee()
         {
             Employee = null;
+            AuthToken = string.Empty;
         }
 
+        /// <summary>
+        /// Determines whether the current user is logged in.
+        /// </summary>
+        /// <precondition>true</precondition>
+        /// <postcondition>true</postcondition>
+        /// <returns>
+        ///   <c>true</c> if logged in; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsLoggedIn()
+        {
+            return Employee != null;
+        }
+
+        /// <summary>
+        /// Determines whether the current user is an admin.
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this instance is admin; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsAdmin()
+        {
+            return IsLoggedIn() && (Employee.IsAdmin ?? false);
+        }
+
+        /// <summary>
+        /// Verifies the validity of a request based on the specified token.
+        /// </summary>
+        /// <precondition>true</precondition>
+        /// <postcondition>true</postcondition>
+        /// <param name="authToken">The authentication token.</param>
+        /// <returns><c>True</c> if the request is valid, <c>false</c> otherwise.</returns>
+        public static bool ValidateRequestToken(string authToken)
+        {
+            return IsLoggedIn() && !authToken.IsNullOrEmpty() && AuthToken.Equals(authToken);
+        }
+
+        private static void generateAuthToken(Random rng)
+        {
+            string result = string.Empty;
+            for (int i = 0; i < AUTH_TOKEN_LENGTH; i++)
+            {
+                result += (char)rng.Next();
+            }
+            AuthToken = result;
+        }
     }
 }
