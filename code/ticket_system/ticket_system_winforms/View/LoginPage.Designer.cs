94c05cc46fcd20523555cb70fae632894b3fdb69
Stefan <ssrrtteeee@gmail.com>
2025-04-10 03:05:42 -0400

    Created Initial Login Page With Base Functionality
    * Uses errorProviders to display errors.
    * Should update some of the names.

---
+++ code/ticket_system/ticket_system_winforms/View/LoginPage.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ticket_system_winforms.View
+{
+    partial class LoginPage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            login_button = new System.Windows.Forms.Button();
+            userNameTextField = new System.Windows.Forms.TextBox();
+            passwordTextField = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            errorProvider1 = new System.Windows.Forms.ErrorProvider(components);
+            errorProvider2 = new System.Windows.Forms.ErrorProvider(components);
+            errorProvider3 = new System.Windows.Forms.ErrorProvider(components);
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider3).BeginInit();
+            SuspendLayout();
+            // 
+            // login_button
+            // 
+            login_button.Location = new System.Drawing.Point(357, 207);
+            login_button.Name = "login_button";
+            login_button.Size = new System.Drawing.Size(75, 23);
+            login_button.TabIndex = 0;
+            login_button.Text = "Log In";
+            login_button.UseVisualStyleBackColor = true;
+            login_button.Click += login_button_Click;
+            // 
+            // userNameTextField
+            // 
+            userNameTextField.Location = new System.Drawing.Point(346, 94);
+            userNameTextField.Name = "userNameTextField";
+            userNameTextField.Size = new System.Drawing.Size(100, 23);
+            userNameTextField.TabIndex = 1;
+            // 
+            // passwordTextField
+            // 
+            passwordTextField.Location = new System.Drawing.Point(346, 160);
+            passwordTextField.Name = "passwordTextField";
+            passwordTextField.Size = new System.Drawing.Size(100, 23);
+            passwordTextField.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(346, 67);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(60, 15);
+            label1.TabIndex = 3;
+            label1.Text = "Username";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(346, 133);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(57, 15);
+            label2.TabIndex = 4;
+            label2.Text = "Password";
+            // 
+            // errorProvider1
+            // 
+            errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            errorProvider3.ContainerControl = this;
+            // 
+            // LoginPage
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(passwordTextField);
+            Controls.Add(userNameTextField);
+            Controls.Add(login_button);
+            Name = "LoginPage";
+            Text = "LoginPage";
+            ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)errorProvider3).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button login_button;
+        private System.Windows.Forms.TextBox userNameTextField;
+        private System.Windows.Forms.TextBox passwordTextField;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+    }
+}
---
+++ code/ticket_system/ticket_system_winforms/View/LoginPage.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ticket_system_web_app.Data;
+using ticket_system_web_app.Models;
+
+namespace ticket_system_winforms.View
+{
+    public partial class LoginPage : Form
+    {
+        private readonly TicketSystemDbContext context;
+        private static string VALIDATE_PARAMETER_MESSAGE = "input must not be null or empty";
+        private static string INVALID_ENTERED_CREDENTIALS = "credentials entered are invalid";
+
+        public LoginPage(TicketSystemDbContext context)
+        {
+            InitializeComponent();
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context), VALIDATE_PARAMETER_MESSAGE);
+            }
+            this.context = context;
+        }
+
+        private void login_button_Click(object sender, EventArgs e)
+        {
+            String username = userNameTextField.Text;
+            String password = passwordTextField.Text;
+            Boolean invalidLogin = false;
+
+            if (string.IsNullOrEmpty(username))
+            {
+                this.errorProvider1.SetError(label1, "Username cannot be empty.");
+                invalidLogin = true;
+            }
+            else
+            {
+                this.errorProvider1.SetError(label1, string.Empty);
+            }
+
+            if (string.IsNullOrEmpty(password))
+            {
+                this.errorProvider2.SetError(label2, "Password cannot be empty.");
+                invalidLogin = true;
+            }
+            else
+            {
+                this.errorProvider2.SetError(label2, string.Empty);
+            }
+            if (invalidLogin)
+            {
+                this.errorProvider3.SetError(login_button, string.Empty);
+                return;
+            }
+
+            Employee user = this.context.Employees.FirstOrDefault(employee => employee.Username != null && employee.Username.Equals(username));
+            var userPassword = user?.HashedPassword;
+            if (user == null || userPassword == null || !this.verifyCorrectPassword(password, userPassword))
+            {
+                this.errorProvider3.SetError(login_button, "Invalid Credentials.");
+                invalidLogin = true;
+            }
+
+            if (!invalidLogin)
+            {
+                Form form = new HomeForm();
+                form.Show();
+                this.Hide();
+            } else
+            {
+                return;
+            }
+        }
+
+        private bool verifyCorrectPassword(string passwordEntered, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(passwordEntered, hashedPassword);
+        }
+    }
+}
